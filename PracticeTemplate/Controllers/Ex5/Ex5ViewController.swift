37e60ce8d614401a7ba3a1d9fb4d908136849880
Mark Nguyen <tringuyenn1809@gmail.com>
2020-08-19 23:16:15 +0700

    Touch View

--- PracticeTemplate/Controllers/Ex5/Ex5ViewController.swift
+++ PracticeTemplate/Controllers/Ex5/Ex5ViewController.swift
@@ -10,13 +10,36 @@ import UIKit
 
 class Ex5ViewController: BaseViewController {
     
+    
+    @IBOutlet private weak var ballview: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupBall()
     }
+    func setupBall() {
+        ballview.bounds.size = CGSize(width: 100, height: 100)
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            ballview.frame.origin.x = touch.location(in: view).x - ballview.frame.width / 2
+            ballview.frame.origin.y = touch.location(in: view).y - ballview.frame.height / 2
+        }
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            ballview.frame.origin.x = touch.location(in: view).x - ballview.frame.width / 2
+            ballview.frame.origin.y = touch.location(in: view).y - ballview.frame.height / 2
+        }
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    
+    
     
     // MARK: config
     override func setupUI() {
